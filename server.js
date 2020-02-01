b5fc4ecbe0965070347064c2175b5100bfe68879
arigato <teeruch.k@gmail.com>
2020-02-01 17:42:23 +0700

    Express setup

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+// Load env vars objects (โหลดจากไฟล์ไม่ได้ config ในไฟล์กำหนด path)
+dotenv.config({ path: './config/config.env' });
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
