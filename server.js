293cbc709dfd6ee0ba483a5ca95a71a2a03ab456
arigato <teeruch.k@gmail.com>
2020-02-01 18:05:41 +0700

    Create Routes and Responses

--- server.js
+++ server.js
@@ -6,17 +6,36 @@ dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
-app.get('/', (req, res) => {
-  // res.send({ name: 'John Doe' });
+// ใช้ route เดียวกัน แต่ต่าง method
+// *GET all bootcamps
+app.get('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({ success: true, msg: 'Show all bootcamps' });
+});
 
-  // * send json object
-  // res.json({ name: 'John Doe' });
+// *GET one bootcamp
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Show bootcamp id :${req.params.id}` });
+});
 
-  // * send status
-  // res.sendStatus(400);
+// *POST (create) bootcamp
+app.post('/api/v1/bootcamps', (req, res) => {
+  res.status(201).json({ success: true, msg: 'Create new bootcamp' });
+});
 
-  // send status and data
-  res.status(200).json({ success: true, data: { id: 1 } });
+// *UPDATE entry bootcamp (ต้องระบุ :id ที่ route (req.params.id คือการเอาไปใช้งาน))
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Update bootcamp ${req.params.id}` });
+});
+
+// *DELETE bootcamp อย่าลืมเปลี่ยน method ตอน copy
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
 });
 
 const PORT = process.env.PORT || 5000;
