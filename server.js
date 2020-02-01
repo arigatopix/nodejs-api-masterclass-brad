99ad025a01211d61ba025e831e2578fc8e978faf
arigato <teeruch.k@gmail.com>
2020-02-01 17:55:28 +0700

    send data

--- server.js
+++ server.js
@@ -6,6 +6,19 @@ dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
+app.get('/', (req, res) => {
+  // res.send({ name: 'John Doe' });
+
+  // * send json object
+  // res.json({ name: 'John Doe' });
+
+  // * send status
+  // res.sendStatus(400);
+
+  // send status and data
+  res.status(200).json({ success: true, data: { id: 1 } });
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(
