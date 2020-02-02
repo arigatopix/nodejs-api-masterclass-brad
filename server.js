16c6a56dee8bfee823e576e234dd072978039b58
arigato <teeruch.k@gmail.com>
2020-02-02 12:23:51 +0700

    connect to Database with MongoDB

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 
 // * Middlewares
 // ใช้ log method และ status
 const morgan = require('morgan');
 
-// * Route File ของ Stephen จะกำหนด require ที่ Mount routers เลย
-const bootcamps = require('./routes/bootcamps');
-
 // * Load env vars objects (โหลดจากไฟล์ไม่ได้ config ในไฟล์กำหนด path)
 dotenv.config({ path: './config/config.env' });
 
+// * Route File ของ Stephen จะกำหนด require ที่ Mount routers เลย
+const bootcamps = require('./routes/bootcamps');
+
+// * Init Express
 const app = express();
+
+// * Connect to database
+connectDB();
 
 // use middlewares
 if (process.env.NODE_ENV === 'development') {
@@ -24,7 +29,16 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+// กำหนด variable server
+const server = app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+// * Global Handle unhandled promise rejections (กรณี database หรืออื่นๆ มีปัญหาให้แสดง msg)
+// จะใช้ try...cratch block ใน db.js ก็ได้
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Close server & exit process โดยตังเลข 1 ปิด server จาก code ผิดพลาด
+  server.close(() => process.exit(1));
+});
