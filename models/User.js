6a4887de555f305347af381da9461bfad92aaa02
arigatopix <teeruch.k@gmail.com>
2020-02-18 11:27:43 +0700

    User Register&Encrypt Password

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -36,6 +37,15 @@ const UserSchema = new mongoose.Schema({
     type: Date,
     default: Date.now
   }
+});
+
+// *Encrypt password using bcrypt
+UserSchema.pre('save', async function(next) {
+  const salt = await bcrypt.genSalt(10);
+  // เก็บ hash ลง db
+  this.password = await bcrypt.hash(this.password, salt);
+
+  next();
 });
 
 module.exports = mongoose.model('User', UserSchema);
